562d73400219d0e72223aae8189cfc782e4e8441
nimomaina <nimomaina.nm@gmail.com>
2019-02-17 22:49:06 +0300

    Create models for users and blog

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
+
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+from flask_login import UserMixin, current_user
+from . import login_manager
+
+
 class Quote:
     '''
     Quote class to define random quotes
@@ -5,4 +12,71 @@ class Quote:
 
     def __init__(self, quote):
         self.quote = quote
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvotes = db.relationship('Upvote', backref='user', lazy='dynamic')
+    downvotes = db.relationship('Downvote', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    def __repr__(self):
+        return f'{self.username}'
+
+
+class Pitch(db.Model):
+    '''
+    '''
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    description = db.Column(db.String(), index=True)
+    title = db.Column(db.String())
+    category = db.Column(db.String(255), nullable=False)
+    comments = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    upvotes = db.relationship('Upvote', backref='pitch', lazy='dynamic')
+    downvotes = db.relationship('Downvote', backref='pitch', lazy='dynamic')
+
+    @classmethod
+    def get_pitches(cls, id):
+        pitches = Pitch.query.order_by(pitch_id=id).desc().all()
+        return pitches
+
+    def __repr__(self):
+        return f'Pitch {self.description}'
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    description = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"Comment : id: {self.id} comment: {self.description}"
 
